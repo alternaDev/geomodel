8dc8f72ac0e3bde327c4cc4cb807b06105529719
Jan-Henrik Bruhn <jh.bruhn@me.com>
2016-06-09 14:43:30 +0200

    Fixed geohash implementation, added it under the correct name

--- geomodel_test.go
+++ geomodel_test.go
@@ -1,7 +1,10 @@
 package geomodel
 
-import "testing"
-import "sort"
+import (
+	"log"
+	"sort"
+	"testing"
+)
 
 /*type LocationCapable interface {
 	Latitude() float64
@@ -11,61 +14,65 @@ import "sort"
 }*/
 
 type Place struct {
-  lat, lon float64
-  key string
-  geocells []string
+	lat, lon float64
+	key      string
+	geocells []string
 }
 
 func (p Place) Latitude() float64 {
-  return p.lat
+	return p.lat
 }
 
 func (p Place) Longitude() float64 {
-  return p.lon
+	return p.lon
 }
 
 func (p Place) Key() string {
-  return p.key
+	return p.key
 }
 
 func (p Place) Geocells() []string {
-  return p.geocells
+	return p.geocells
+}
+
+func TestGeoHash(t *testing.T) {
+	log.Printf("GeoHash: %s", GeoCell(53.12869, 8.18976, 6))
 }
 
 func TestProximityFetch(t *testing.T) {
 
-  // Example Places
-  var places []LocationCapable = []LocationCapable {Place{54, 8, "1", GeoCells(54, 8, 10)},Place{50, 8, "2", GeoCells(50, 8, 10)},Place{49, 8, "3", GeoCells(49, 8, 10)},Place{48, 8, "4", GeoCells(48, 8, 10)},Place{47, 8, "5", GeoCells(47, 8, 10)}}
+	// Example Places
+	var places = []LocationCapable{Place{54, 8, "1", GeoCells(54, 8, 10)}, Place{50, 8, "2", GeoCells(50, 8, 10)}, Place{49, 8, "3", GeoCells(49, 8, 10)}, Place{48, 8, "4", GeoCells(48, 8, 10)}, Place{47, 8, "5", GeoCells(47, 8, 10)}}
 
-  var result []LocationCapable = ProximityFetch(50, 8, 20, 300, func(cells []string) []LocationCapable {
-    // Implementing our own dirty thing here.
-    var result []LocationCapable = make([]LocationCapable, 0)
+	var result = ProximityFetch(50, 8, 20, 300, func(cells []string) []LocationCapable {
+		// Implementing our own dirty thing here.
+		var result []LocationCapable = make([]LocationCapable, 0)
 
-    for _, place := range places {
-      var added bool = false
-      for _, c := range place.Geocells() {
-        index := sort.SearchStrings(cells, c)
-        if index < len(cells) {
-          if place != nil {
-            result = append(result, place)
-          }
-        }
-      }
-      if added {
-        break
-      }
-    }
+		for _, place := range places {
+			var added bool = false
+			for _, c := range place.Geocells() {
+				index := sort.SearchStrings(cells, c)
+				if index < len(cells) {
+					if place != nil {
+						result = append(result, place)
+					}
+				}
+			}
+			if added {
+				break
+			}
+		}
 
-    return result
-  }, 10)
+		return result
+	}, 10)
 
-  if len(result) > 1 {
-    t.Fail()
-  }
+	if len(result) > 1 {
+		t.Fail()
+	}
 
-  if(result[0].Latitude() != 50 || result[0].Longitude() != 8) {
-    t.Fail()
-  }
+	if result[0].Latitude() != 50 || result[0].Longitude() != 8 {
+		t.Fail()
+	}
 
-  // ProximityFetch(lat, lon float64, maxResults int, maxDistance float64, search RepositorySearch, maxResolution int) []LocationCapable
+	// ProximityFetch(lat, lon float64, maxResults int, maxDistance float64, search RepositorySearch, maxResolution int) []LocationCapable
 }
